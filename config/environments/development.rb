9bbbe10b2647dff40d7aeb3bf966977c0207072b
mimipeshy <perisndanu@gmail.com>
2023-03-17 13:51:45 +0300

    allow ngrok to environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  #allow accees to rails app via ngrok
+  config.hosts << /[a-z0-9]+\.ngrok\.io/
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
