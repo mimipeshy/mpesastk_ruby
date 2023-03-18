f3407fd718b3766e853b854d23876f490102d19b
mimipeshy <perisndanu@gmail.com>
2023-03-18 23:40:14 +0300

    create tokens

--- app/controllers/mpesas_controller.rb
+++ app/controllers/mpesas_controller.rb
@@ -5,50 +5,50 @@ class MpesasController < ApplicationController
     # stkpush
     def stkpush
         phoneNumber = params[:phoneNumber]
-    amount = params[:amount]
+        amount = params[:amount]
 
-    url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
-    timestamp = "#{Time.now.strftime "%Y%m%d%H%M%S"}"
-    business_short_code = ENV["MPESA_SHORTCODE"]
-    password = Base64.strict_encode64("#{business_short_code}#{ENV["MPESA_PASSKEY"]}#{timestamp}")
-    
-    payload = {
-      'BusinessShortCode': business_short_code,
-			'Password': password,
-			'Timestamp': timestamp,
-			'TransactionType': "CustomerPayBillOnline",
-			'Amount': amount,
-			'PartyA': phoneNumber,
-			'PartyB': business_short_code,
-			'PhoneNumber': phoneNumber,
-			'CallBackURL': "#{ENV["CALLBACK_URL"]}/callback_url",
-			'AccountReference': 'Trial ROR Mpesa',
-			'TransactionDesc': "ROR trial"
-    }.to_json
-    
-    headers = {
-      content_type: 'application/json', 
-      Authorization:"Bearer #{get_access_token}"
-    }
-    response = RestClient::Request.new({
-      method: :post,
-      url: url,
-      payload: payload,
-      headers: headers
-      }).execute do |response, request|
-        case response.code
-        when 500
-          [ :error, JSON.parse(response.to_str) ]
-        when 400
-          [ :error, JSON.parse(response.to_str) ]
-        when 200
-          [ :success, JSON.parse(response.to_str) ]
-        else
-          fail "Invalid response #{response.to_str} received."
+        url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+        timestamp = "#{Time.now.strftime "%Y%m%d%H%M%S"}"
+        business_short_code = ENV["MPESA_SHORTCODE"]
+        password = Base64.strict_encode64("#{business_short_code}#{ENV["MPESA_PASSKEY"]}#{timestamp}")
+        
+        payload = {
+        'BusinessShortCode': business_short_code,
+                'Password': password,
+                'Timestamp': timestamp,
+                'TransactionType': "CustomerPayBillOnline",
+                'Amount': amount,
+                'PartyA': phoneNumber,
+                'PartyB': business_short_code,
+                'PhoneNumber': phoneNumber,
+                'CallBackURL': "#{ENV["CALLBACK_URL"]}/callback_url",
+                'AccountReference': 'GradeOne',
+                'TransactionDesc': "GradeOne Trial"
+        }.to_json
+        
+        headers = {
+        content_type: 'application/json', 
+        Authorization:"Bearer #{get_access_token}"
+        }
+        response = RestClient::Request.new({
+        method: :post,
+        url: url,
+        payload: payload,
+        headers: headers
+        }).execute do |response, request|
+            case response.code
+            when 500
+            [ :error, JSON.parse(response.to_str) ]
+            when 400
+            [ :error, JSON.parse(response.to_str) ]
+            when 200
+            [ :success, JSON.parse(response.to_str) ]
+            else
+            fail "Invalid response #{response.to_str} received."
+            end
         end
-      end
-    
-    render json: response
+        
+        render json: response
     end
 
     private
