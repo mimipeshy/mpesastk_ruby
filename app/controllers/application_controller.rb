59dec81f6cb92906f1442eb60d80ddcca60a9e54
mimipeshy <perisndanu@gmail.com>
2023-03-18 18:39:35 +0300

    generate access token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+    private
+    #Gives you a time bound access token to call allowed APIs in the sandbox.
+    def generate_access_token_request
+        @url= "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+        @consumer_key=  ENV['MPESA_CONSUMER_KEY']
+        @consumer_secret= ENV['MPESA_CONSUMER_SECRET']
+        @userpass = Base64::strict_encode64("#{@consumer_key}:#{@consumer_secret}")
+        headers ={
+            Authorization : "Bearer #{@userpass}"
+        }
+        res = RestClient::Request.execute(url: @url, method: :get, headers:{
+            Authorization: "Basic #{@userpass}"
+        })
+        res
+    end
+
+    #Used to check if generate_acces_token_request is 
+    #successful or not then it reads the responses and 
+    #extracts the access token from the response and 
+    #saves it to the database.
+    def get_access_token
+    end
 end
--- app/controllers/mpesas_controller.rb
+++ app/controllers/mpesas_controller.rb
@@ -1,2 +1,4 @@
+require 'rest-client'
+
 class MpesasController < ApplicationController
 end
